f9ac7e0510cdd5d2cc0b93cfbfdcb78480d78083
victor <vgrusz@yahoo.com.ar>
2022-02-19 00:27:28 +0100

    Alfa funcional

--- test/test.js
+++ test/test.js
@@ -1,3 +1,12 @@
-const rellenar = require("./seedDB");
+const chai​ = require​("chai"​);
 
-rellenar();
+const assert = chai.assert;
+
+describe(​ 'Array'​ , ​ function​ () {
+describe(​ '#indexOf()'​ , ​ function​ () {
+it(​ 'should return -1 when the value is not present'​ , ​ function​ () {
+    assert​.equal([​ 1 ​ , ​ 2 ​ , ​ 3 ​ ].indexOf(​ 4 ​ ), ​ -1​ );
+
+});
+});
+});

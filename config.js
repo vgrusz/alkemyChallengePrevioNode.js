81d9aa53c7cdf66cc794a94921aa1ed622baee8b
victor <vgrusz@yahoo.com.ar>
2022-02-22 21:08:44 +0100

    Alfa funcional

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 module.exports = {
   database: {
+    /*
     username: "root",
     password: "",
     database: "disney",
     host: "localhost",
-    /*
+    */
     username: "7YRMOZ6rAx",
     password: process.env.DBPASSWORD,
     database: "7YRMOZ6rAx",
-    host: "remotemysql.com",*/
+    host: "remotemysql.com",
   },
 };
